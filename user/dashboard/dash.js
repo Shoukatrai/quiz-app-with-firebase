c2e7ca7d85cb25a2ee56e6eeae08513541e9a2b2
Shoukatrai <raishoukat033@gmail.com>
2025-02-05 14:59:44 +0500

    complete

--- user/dashboard/dash.js
+++ user/dashboard/dash.js
@@ -1,3 +1,5 @@
+import { collection, db, getDocs } from "../../firebase.js"
+
 const logOut = ()=>{
     localStorage.clear()
     window.location.replace("../../index.html")
@@ -11,8 +13,29 @@ const adminCheck = ()=>{
     }
 }
 
+const parent = document.querySelector(".parent")
+const showQuizDetail =async ()=>{
+    try {
+        const quiz = await getDocs(collection(db , "quizzes"))
+        quiz.forEach((doc)=>{
+            const quizData = doc.data()
+            console.log(quizData)
+            parent.innerHTML += `<div class="card">
+            <h1 id="quizTitle"> ${quizData.title} </h1>
+            <h2 id="quizCategory"> ${quizData.category}  </h2>
+            <h3 id="quizDesc"> ${quizData.desc}  </h3>
+        </div>`
+        })
+       
+    } catch (error) {
+        console.log(error)
+    }
+
+
+}
 
 
 
+window.showQuizDetail = showQuizDetail
 window.adminCheck = adminCheck
 window.logOut = logOut
